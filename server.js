9b0ebf89a81b444666fb9930fd36bb14c24dd552
joshuamccluskey <jpiff57@gmail.com>
2022-02-01 10:27:49 -0800

    Server working; ENV Vars are complet; ENV SAMPLE ADD; Blank Build out files added hero.js and seed.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const axios = require('axios');
 
-mongoose.connect(process.env.DB_URL)
+// mongoose.connect(process.env.DB_URL)
 
 const app = express();
 app.use(cors());
@@ -17,30 +17,30 @@ const PORT = process.env.PORT || 3001;
 const verifyUser = require('./auth.js');
 
 
-app.get('/user', handelGetUser);
+// app.get('/user', handelGetUser);
 
-verifyUser(req, async (err, user) => {
-  if (err) {
-    console.error(err);
-    res.send('invalid token');
-  } else {
-    // insert try catch logic here.  BE CAREFUL.  check syntax IMMEDIATELY
-    try {
-      //return all the results with empty object or get books from the same user email
-      let herosFromDb = await Hero.find({ email: user.email });
+// verifyUser(req, async (err, user) => {
+//   if (err) {
+//     console.error(err);
+//     res.send('invalid token');
+//   } else {
+//     // insert try catch logic here.  BE CAREFUL.  check syntax IMMEDIATELY
+//     try {
+//       //return all the results with empty object or get books from the same user email
+//       let herosFromDb = await Hero.find({ email: user.email });
 
-      if (herosFromDb.length > 0) {
-        res.status(200).send(herosFromDb);
-      } else {
-        res.status(404).send('No books found...☹️');
-      }
-    } catch (err) {
-      res.status(500).send('Server Error...😩');
-    }
-  }
-});
+//       if (herosFromDb.length > 0) {
+//         res.status(200).send(herosFromDb);
+//       } else {
+//         res.status(404).send('No books found...☹️');
+//       }
+//     } catch (err) {
+//       res.status(500).send('Server Error...😩');
+//     }
+//   }
+// });
 
-function handelGetUser(req, res) {
+function handleGetUser(req, res) {
   verifyUser(req, (err, user) => {
     if (err) {
       console.log(err);
@@ -54,6 +54,12 @@ function handelGetUser(req, res) {
 app.get('/test', (request, response) => {
 
   response.send('test request received')
+
+})
+
+app.get('/', (request, response) => {
+
+  response.send('Servers Up 🐉')
 
 })
 
